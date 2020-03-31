13956a47143c68442ffe650e7190e725c0acf4f8
purplesmile3 <puritychepkurui5@gmail.com>
2020-03-31 17:40:21 +0300

    Update app with components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React,{ Fragment} from 'react';
 
 import './App.css';
+//components
+import InputTodo from './components/inputTodo';
+import ListTodo from './components/ListTodo';
 
 function App() {
-  return <Fragment></Fragment>;
+  return <Fragment>
+    <div className="container">
+    <InputTodo/>
+    <ListTodo/>
+    </div>
+    </Fragment>;
 }
 
 export default App;
