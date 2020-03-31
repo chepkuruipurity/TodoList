8c9d32d8438a46f71eb2e83858418c706f5d7ead
purplesmile3 <puritychepkurui5@gmail.com>
2020-03-31 17:41:10 +0300

    Updating sever

--- server/index.js
+++ server/index.js
@@ -52,11 +52,10 @@ app.put('/todos/:id',async(req,res)=>{
     }
 })
 //delete a todo
-app.delete('todos/:id',async(req,res)=>{
+app.delete('/todos/:id',async(req,res)=>{
     try {
         const {id}= req.params;
-        const deleteTodo= await pool.query(`DELETE * FROM todo
-         WHERE todo_id=$1`,[id]);
+        const deleteTodo= await pool.query(`DELETE FROM todo WHERE todo_id=$1`,[id]);
          res.json('Todo was deleted');
     } catch (err) {
         console.error(err.message);
